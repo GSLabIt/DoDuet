c8367a3f9a12279b9e90c778d0d15387c90a18f8
LotteriniValerio <valerio.lotterini@dolabs.studio>
2021-09-19 16:54:16 +0200

    Models creation - functionalities

---
+++ database/migrations/2019_12_14_000002_create_functionalities_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFunctionalitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('functionalities', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("name");
+            $table->longText("description");
+            $table->boolean("controller")->default(false);
+            $table->boolean("ui")->default(false);
+            $table->foreingUuid("platform");
+            $table->boolean("testing")->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('functionalities');
+    }
+}
