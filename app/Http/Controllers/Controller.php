7396ac93bd7a4d2b675153e741176baa75f3ed98
ebalo <e.balsamo000@gmail.com>
2021-09-20 09:11:08 +0200

    easy-crud inclusion and setup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Ebalo\EasyCRUD\EasyCRUD;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +10,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use EasyCRUD;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
