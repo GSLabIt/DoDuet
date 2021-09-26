2ee4af59d3a178b98eb386f3a5c5dd9035f9042b
maforn <matteo.fornaini@dolabs.studio>
2021-09-26 10:28:07 -0700

    Created, modified and linked Wallet Model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -137,4 +137,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Lyrics::class, "creator_id");
     }
+
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(Wallet::class);
+    }
 }
