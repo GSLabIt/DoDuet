29366fc45337c7c327d6fe8121777dfd86d9b418
ebalo <tuema55@gmail.com>
2021-09-29 02:51:49 -0700

    helpers fix

--- app/Helpers/index.php
+++ app/Helpers/index.php
@@ -1,9 +1,7 @@
 <?php
 
 require_once("directoryFromClass.php");
-require_once("extractFromRelation.php");
 require_once("dropArrKeys.php");
-require_once("simpleJSONError.php");
 require_once("settings.php");
 require_once("mentions.php");
 require_once("sodium.php");
