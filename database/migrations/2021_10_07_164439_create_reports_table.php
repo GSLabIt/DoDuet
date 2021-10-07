2f7310ac07c5e64996391a59c181625a893d89c8
maforn <matteo.fornaini@dolabs.studio>
2021-10-07 09:55:52 -0700

    Creates, modified and linked Reports Model

---
+++ database/migrations/2021_10_07_164439_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->uuidMorphs("reportable");
+            $table->foreignUuid("reason_id"); //->references("id")->on("report_reasons")
+            $table->longText('extra_informations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
