c8070f2d79f2a49ea57b20bcb7b79b85939434a4
LotteriniValerio <valerio.lotterini@dolabs.studio>
2021-10-09 08:50:49 -0700

    created personal_libraries_tracks migration

--- app/Models/PersonalLibraries.php
+++ app/Models/PersonalLibraries.php
@@ -7,6 +7,7 @@ use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class PersonalLibraries extends Model
@@ -18,5 +19,10 @@ class PersonalLibraries extends Model
     function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, "owner_id");
+    }
+
+    function tracks(): BelongsToMany
+    {
+        return $this->belongsToMany(Tracks::class, "personal_libraries_tracks","library_id","track_id");
     }
 }
