f3a805b656fb6cb27a347818bc36465eb413b466
maforn <matteo.fornaini@dolabs.studio>
2021-10-11 09:57:47 -0700

    Updated Referred Factory, already modified with Referral::factory(), to be fully functional merge of referral branch required

--- database/factories/ReferredFactory.php
+++ database/factories/ReferredFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Referred;
+use App\Models\User;
+use App\Models\Referral;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ReferredFactory extends Factory
@@ -22,7 +24,24 @@ class ReferredFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "id" => $this->faker->uuid(),
+            "referrer_id" => Referral::factory(),
+            "referred_id" => User::factory(),
+            "is_redeemed" => $this->faker->boolean(0),
         ];
+    }
+
+    /**
+     * Switch is_redeemed to true.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function redeemed()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                "is_redeemed" => $this->faker->boolean(100),
+            ];
+        });
     }
 }
