ae8c0ccdc32eab3d976ceb732e018414d9feb141
maforn <matteo.fornaini@dolabs.studio>
2021-10-11 10:29:37 -0700

    Updated Test Factory

--- database/factories/TestFactory.php
+++ database/factories/TestFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Functionalities;
+use App\Models\Questionnaire;
 use App\Models\Test;
+use App\Models\UserSegments;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TestFactory extends Factory
@@ -22,7 +25,10 @@ class TestFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "functionality_id" => Functionalities::factory(),
+            "user_segment_id" => UserSegments::factory(),
+            "id" => $this->faker->uuid(),
+            "questionnaire_id" => Questionnaire::factory(),
         ];
     }
 }
