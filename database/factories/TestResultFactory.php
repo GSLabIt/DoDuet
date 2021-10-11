0638fba4dd89dc31fc03be6639795ffc8ff9d9c8
maforn <matteo.fornaini@dolabs.studio>
2021-10-11 10:36:48 -0700

    Updated TestResult Factory

--- database/factories/TestResultFactory.php
+++ database/factories/TestResultFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Test;
 use App\Models\TestResult;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TestResultFactory extends Factory
@@ -22,7 +24,25 @@ class TestResultFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "id" => $this->faker->uuid(),
+            "tester_id" => User::factory(),
+            "utilizations" => $this->faker->numberBetween(0, 18446744073709551615),
+            "has_answered_questionnaire" => $this->faker->boolean(0),
+            "test_id" => Test::factory(),
         ];
+    }
+
+    /**
+     * Switch has_answered_questionnaire to true.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function hasAnsweredQuestionnaire()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                "has_answered_questionnaire" => $this->faker->boolean(100),
+            ];
+        });
     }
 }
