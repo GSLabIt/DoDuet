8b5a28cb3b00a42ffc1cbd8c98286f1c41adad0f
ebalo <e.balsamo000@gmail.com>
2021-10-14 08:03:46 +0200

    generate a link with the referral code

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class ReferralController extends Controller
 {
-    //
+    /**
+     * Return a value for the field.
+     *
+     * @param null $root Always null, since this field has no parent.
+     * @param array<string, mixed> $args The field arguments passed by the client.
+     * @param GraphQLContext $context Shared between all fields.
+     * @param ResolveInfo $resolveInfo Metadata for advanced query resolution.
+     * @return string
+     */
+    public function getReferralURL($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): string
+    {
+        // User won't ever be null here as in the query definition we check it is authenticated using the @guard
+        // directive
+        /**@var User $user*/
+        $user = $context->user();
+
+        return route("register", ["ref" => $user->referral->code]);
+    }
 }
