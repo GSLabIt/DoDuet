cefdf87cd6a39001965e6c62a4771abb6dc9fc91
maforn <matteo.fornaini@dolabs.studio>
2021-10-16 05:25:14 -0700

    Modified UserSegments Factory

--- database/factories/UserSegmentsFactory.php
+++ database/factories/UserSegmentsFactory.php
@@ -22,7 +22,8 @@ class UserSegmentsFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "id" => $this->faker->uuid(),
+            "name" => $this->faker->text(255),
         ];
     }
 }
