bf8e69633c5fba87d72235174c4fbbae02dfc395
maforn <fornainimatteo@gmail.com>
2022-02-10 05:58:42 -0800

    Initial commit of setting controller with graphql

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Exceptions\SettingSafeException;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class SettingsController extends Controller
 {
-    //
+    /**
+     * This function retrieves and return the server public key
+     *
+     * @param null $root Always null, since this field has no parent.
+     * @param array<string, mixed> $args The field arguments passed by the client.
+     * @param GraphQLContext $context Shared between all fields.
+     * @param ResolveInfo $resolveInfo Metadata for advanced query resolution.
+     * @return string
+     */
+    public function getServerPublicKey($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): string {
+        return env("SERVER_PUBLIC_KEY");
+    }
+
+    /**
+     * This function retrieves and return the user private key
+     *
+     * @param null $root Always null, since this field has no parent.
+     * @param array<string, mixed> $args The field arguments passed by the client.
+     * @param GraphQLContext $context Shared between all fields.
+     * @param ResolveInfo $resolveInfo Metadata for advanced query resolution.
+     * @return string
+     */
+    public function getUserSecretKey($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): string {
+        $user = $context->user;
+        return secureUser($user)->get(secureUser($user)->whitelistedItems()["secret_key"]);
+    }
+
+    /**
+     * This function retrieves and return the user public key of a specified user
+     *
+     * @param null $root Always null, since this field has no parent.
+     * @param array<string, mixed> $args The field arguments passed by the client.
+     * @param GraphQLContext $context Shared between all fields.
+     * @param ResolveInfo $resolveInfo Metadata for advanced query resolution.
+     * @return string
+     * @throw SettingSafeException
+     * @throw ValidationException
+     */
+    public function getUserPublicKey($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): string {
+        Validator::validate($args, [
+            "id" => "required|uuid|exists:users,id",
+        ]);
+
+        /** @var User $user */
+        $user = User::whereId($args["user"])->first();
+
+        if(!is_null($user)) {
+            return secureUser($user)->get(secureUser($user)->whitelistedItems()["public_key"]);
+        }
+
+        throw new SettingSafeException(
+            config("error-codes.USER_NOT_FOUND.message"),
+            config("error-codes.USER_NOT_FOUND.code")
+        );
+    }
 }
