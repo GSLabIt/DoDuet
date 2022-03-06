3f8352fae22ed75cef76e8ec08218d2c0dbc83aa
ebalo55 <ebalo55@users.noreply.github.com>
2022-03-06 15:09:00 +0100

    commander self install yet uncompleted, stubs drop missing

--- modules/Commander/src/Console/CommanderSelfInstall.php
+++ modules/Commander/src/Console/CommanderSelfInstall.php
@@ -3,8 +3,9 @@
 namespace Doinc\Modules\Commander\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
 use Spatie\Regex\Regex;
-use Spatie\Emoji\Emoji;
+use Nwidart\Modules\Commands;
 
 class CommanderSelfInstall extends Command
 {
@@ -38,22 +39,65 @@ class CommanderSelfInstall extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $this->info("Overwriting default module configuration ...");
         $commands = [
-
+            \Doinc\Modules\Commander\Console\ModuleCommand::class,
+            \Doinc\Modules\Commander\Console\ModuleController::class,
+            Commands\DisableCommand::class,
+            Commands\DumpCommand::class,
+            Commands\EnableCommand::class,
+            \Doinc\Modules\Commander\Console\ModuleEvent::class,
+            \Doinc\Modules\Commander\Console\ModuleJob::class,
+            \Doinc\Modules\Commander\Console\ModuleListener::class,
+            \Doinc\Modules\Commander\Console\ModuleMail::class,
+            \Doinc\Modules\Commander\Console\ModuleMiddleware::class,
+            \Doinc\Modules\Commander\Console\ModuleNotification::class,
+            \Doinc\Modules\Commander\Console\ModuleProvider::class,
+            \Doinc\Modules\Commander\Console\ModuleInstall::class,
+            Commands\ListCommand::class,
+            // TODO: empower composer psr4 module removal
+            Commands\ModuleDeleteCommand::class,
+            \Doinc\Modules\Commander\Console\ModuleFactory::class,
+            \Doinc\Modules\Commander\Console\ModulePolicy::class,
+            \Doinc\Modules\Commander\Console\ModuleRule::class,
+            Commands\MigrateCommand::class,
+            Commands\MigrateRefreshCommand::class,
+            Commands\MigrateResetCommand::class,
+            Commands\MigrateRollbackCommand::class,
+            Commands\MigrateStatusCommand::class,
+            \Doinc\Modules\Commander\Console\ModuleMigration::class,
+            \Doinc\Modules\Commander\Console\ModuleModel::class,
+            \Doinc\Modules\Commander\Console\ModulePublishConfig::class,
+            Commands\PublishMigrationCommand::class,
+            Commands\PublishTranslationCommand::class,
+            Commands\SeedCommand::class,
+            \Doinc\Modules\Commander\Console\ModuleSeeder::class,
+            Commands\SetupCommand::class,
+            Commands\UnUseCommand::class,
+            Commands\UpdateCommand::class,
+            Commands\UseCommand::class,
+            \Doinc\Modules\Commander\Console\ModuleTest::class,
         ];
 
-        $rex = '';
+        $rex = "/'commands' => \[\n(.+\n)+\s+],/";
         $config = config_path("modules.php");
-        $replacement = "";
+        $replacement = "'command' => [\n        " .
+            implode(
+                "\n        ",
+                array_map(
+                    fn(string $v) => $v . "::class,",
+                    $commands
+                )
+            ) .
+            "\n    ],";
 
         $content = file_get_contents($config);
-        // $content = Regex::replace($rex, $replacement, $content)->result();
+        $content = Regex::replace($rex, $replacement, $content)->result();
         file_put_contents($config, $content);
 
-        $this->info("Overwrite completed, commander installed and working " . Emoji::winkingFace());
+        $this->info("Overwrite completed, commander installed");
 
         return 0;
     }
